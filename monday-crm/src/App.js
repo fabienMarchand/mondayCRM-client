8fb5f5e626477bc9addb3023dcabd785101f4e25
Marcha <marchaf@gmail.com>
2023-01-09 11:23:58 +0100

    add some basics

--- monday-crm/src/App.js
+++ monday-crm/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-function App() {
+import Dashboard from "./pages/Dashboard";
+import TicketPage from "./pages/TicketPage";
+import Nav from "./components/Nav";
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/ticket" element={<TicketPage />} />
+          <Route path="/ticket/:id" element={<TicketPage editMode={true} />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
